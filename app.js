53f7e65747c8bc5a7a6928127132299ec9cd101d
Edgardo Munoz Ramirez <edgardojmr@gmail.com>
2025-02-01 15:17:05 -0300

    Respaldo que debí enviar ayer 30 de enero

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//array que permitirá almacenar los nombres de los amigos
 let listaDeAmigos=[];
 
-function poblaLista(){
+function agregarAmigo(){
+    listaDeAmigos.push(document.getElementById('amigo').value);
+    document.getElementById('amigo').value='';
+    document.getElementById('amigo').focus();
+    //alert(nombreAmigo);
+}
+
+function asignarTextoElemento(elemento, id, texto){
+    let elementoHTML=document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+}
+
+function sortearAmigo(){
+    for (let i=0; i<= listaDeAmigos.length-1; i++){
+        alert(listaDeAmigos[i]);    
+        document.getElementById('resultado').value=listaDeAmigos[i];
+        let li=document.createComment('li');
+        li.appendChild(listaDeAmigos[i]);
+        asignarTextoElemento('ul', 'resultado', listaDeAmigos[i]);
+    }
     
 }
+
+//console.log(recorreListaAmigos());
