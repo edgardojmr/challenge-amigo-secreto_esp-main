5a282026b9c17374467dba993d1b29e00c0434da
Edgardo Munoz Ramirez <edgardojmr@gmail.com>
2025-02-01 16:33:48 -0300

    Ya se puede ingresar amigos y uestra uno a uno

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //array que permitirá almacenar los nombres de los amigos
 let listaDeAmigos=[];
+let indiceActual=0;
 
 function agregarAmigo(){
-    listaDeAmigos.push(document.getElementById('amigo').value);
-    document.getElementById('amigo').value='';
-    document.getElementById('amigo').focus();
-    //alert(nombreAmigo);
+    if (document.getElementById('amigo').value!=''){
+        listaDeAmigos.push(document.getElementById('amigo').value);//agrego cada nuevo elemento a la lista
+        const ulAmigos = document.getElementById('listaAmigos');
+        if (indiceActual < listaDeAmigos.length) {//luego, verifico si ya se agregaron todos los elementos
+            const liAmigos = document.createElement('li'); //crea un elemento <li>
+            liAmigos.textContent = listaDeAmigos[indiceActual]; //asigno el texto
+            ulAmigos.appendChild(liAmigos); //agrego el <li> al <ul>
+            console.log('Último elemento insertado:', listaDeAmigos[indiceActual]);//se muestra el último elemento insertado
+            indiceActual++; //incremento el índice para el próximo elemento
+        } else {
+            console.log('Todos los elementos ya han sido agregados.');
+        }
+        document.getElementById('amigo').value='';
+        document.getElementById('amigo').focus();
+    }else{
+        alert('Por favor, ingresar un nombre válido. Vuelva a intentarlo.')
+    }
 }
 
-function asignarTextoElemento(elemento, id, texto){
-    let elementoHTML=document.querySelector(elemento);
-    elementoHTML.innerHTML=texto;
-}
 
 function sortearAmigo(){
-    for (let i=0; i<= listaDeAmigos.length-1; i++){
-        alert(listaDeAmigos[i]);    
-        document.getElementById('resultado').value=listaDeAmigos[i];
-        let li=document.createComment('li');
-        li.appendChild(listaDeAmigos[i]);
-        asignarTextoElemento('ul', 'resultado', listaDeAmigos[i]);
-    }
-    
+    listaDeAmigos
 }
 
 //console.log(recorreListaAmigos());
